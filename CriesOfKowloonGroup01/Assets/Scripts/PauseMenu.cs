ac2f756b4a43c2d60b0b1ae78701206b1b1c8361
SharifZafran <0128023@kdu-online.com>
2022-02-21 03:01:20 +0800

    updated menu

--- CriesOfKowloonGroup01/Assets/Scripts/PauseMenu.cs
+++ CriesOfKowloonGroup01/Assets/Scripts/PauseMenu.cs
@@ -6,55 +6,98 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GamePaused = false;
+    public static bool MenuOpened = false;
     public GameObject pauseMenuUi;
+    public GameObject MenuImage;
+    public GameObject ComboList;
+    public GameObject ControlScheme;
 
      void Start()
     {
         Debug.Log("Game is not paused");
         GamePaused = false;
+        MenuOpened = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(!GamePaused)
         {
-            if (GamePaused)
-            {
-                Resume();
-            }
-            else
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 Pause();
             }
+            
         }
 
         if (GamePaused)
         {
-            if (Input.GetKeyDown(KeyCode.M))
+            //unpause the game
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Resume();
+            }
+            //goes to main menu
+            if (Input.GetKeyDown(KeyCode.Alpha2))
             {
                 Time.timeScale = 1f;
                 SceneManager.LoadScene(0);
             }
-        }
-        
-    }
+            //shows combo list
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Debug.Log("Combo List is opened");
+                ComboList.SetActive(true);
+                pauseMenuUi.SetActive(false);
+                MenuOpened = true;
 
+            }
+            //shows controls
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Debug.Log("ControlScheme Open");
+                ControlScheme.SetActive(true);
+                pauseMenuUi.SetActive(false);
+                MenuOpened = true;
+
+
+            }
+
+            
+        }
+
+        if (MenuOpened)
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                Debug.Log("Combo List is closed");
+                ComboList.SetActive(false);
+                ControlScheme.SetActive(false);
+                pauseMenuUi.SetActive(true);
+                MenuOpened = false;
+            }
+        }
+    }
 
     void Resume()
     {
-            Debug.Log(" Game Is Not Paused");
-            pauseMenuUi.SetActive(false);
-            Time.timeScale = 1f;
-            GamePaused = false;
+        Debug.Log(" Game Is Not Paused");
+        pauseMenuUi.SetActive(false);
+        MenuImage.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        
     }
 
     void Pause()
     {
-            Debug.Log("Game Is Paused");
-            pauseMenuUi.SetActive(true);
-            Time.timeScale = 0f;
-            GamePaused = true;
+        Debug.Log("Game Is Paused");
+        pauseMenuUi.SetActive(true);
+        MenuImage.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        
     }
 
     
