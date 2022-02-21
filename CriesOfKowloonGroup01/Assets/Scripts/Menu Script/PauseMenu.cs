838dfe4c7c88e2f81f5f491224da080bb529d30a
RandomWinter <85387387+RandomWinter@users.noreply.github.com>
2022-02-21 10:54:39 +0800

    Update

--- CriesOfKowloonGroup01/Assets/Scripts/Menu Script/PauseMenu.cs
+++ CriesOfKowloonGroup01/Assets/Scripts/Menu Script/PauseMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,71 +5,57 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GamePaused;
     public static bool MenuOpened;
+    
     public GameObject pauseMenuUi;
     public GameObject MenuImage;
     public GameObject ComboList;
     public GameObject ControlScheme;
 
-     void Start()
-    {
+     void Start() {
         Debug.Log("Game is not paused");
         GamePaused = false;
         MenuOpened = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(!GamePaused)
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
+    void Update() {
+        if(!GamePaused) {
+            if (Input.GetKeyDown(KeyCode.Return)) {
                 Pause();
             }
-            
         }
 
-        if (GamePaused)
-        {
+        if (GamePaused) {
             //unpause the game
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
                 Resume();
             }
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
+            
+            //goes to main menu
+            if (Input.GetKeyDown(KeyCode.Alpha2)) {
                 Time.timeScale = 1f;
                 SceneManager.LoadScene(0);
             }
-            //goes to main menu
-           
-            //shows combo list
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                Debug.Log("Combo List is opened");
-                ComboList.SetActive(true);
-                pauseMenuUi.SetActive(false);
-                MenuOpened = true;
-
-            }
-            //shows controls
-            if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                Debug.Log("ControlScheme Open");
-                ControlScheme.SetActive(true);
-                pauseMenuUi.SetActive(false);
-                MenuOpened = true;
-
-
-            }
-
             
+            //shows combo list
+            if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                Debug.Log("Combo List is opened");
+                pauseMenuUi.SetActive(false);
+                ComboList.SetActive(true);
+                MenuOpened = true;
+            }
+            
+            //shows controls
+            if (Input.GetKeyDown(KeyCode.Alpha4)) {
+                Debug.Log("ControlScheme Open");
+                pauseMenuUi.SetActive(false);
+                ControlScheme.SetActive(true);
+                MenuOpened = true;
+            }
         }
 
-        if (MenuOpened)
-        {
-            if (Input.GetKeyDown(KeyCode.Backspace))
-            {
+        if (MenuOpened) {
+            if (Input.GetKeyDown(KeyCode.Backspace)) {
                 Debug.Log("Combo List is closed");
                 ComboList.SetActive(false);
                 ControlScheme.SetActive(false);
@@ -79,30 +63,25 @@ public class PauseMenu : MonoBehaviour
                 MenuOpened = false;
             }
         }
-
-
     }
 
-    void Resume()
-    {
+    void Resume() {
         Debug.Log(" Game Is Not Paused");
         pauseMenuUi.SetActive(false);
         MenuImage.SetActive(false);
+        ComboList.SetActive(false);
+        ControlScheme.SetActive(false);
+        
         Time.timeScale = 1f;
         GamePaused = false;
-        
     }
 
-    void Pause()
-    {
+    void Pause() {
         Debug.Log("Game Is Paused");
         pauseMenuUi.SetActive(true);
         MenuImage.SetActive(true);
+        
         Time.timeScale = 0f;
         GamePaused = true;
-        
     }
-
-
-    
 }
