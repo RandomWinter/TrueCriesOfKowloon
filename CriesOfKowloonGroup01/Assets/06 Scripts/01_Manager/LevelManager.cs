a3cc2ec0fec6bcebd80edafe69205f3c240d916b
Chan Wing Shen <0128682@kdu-online.com>
2022-03-21 10:05:37 +0800

    Update

--- CriesOfKowloonGroup01/Assets/06 Scripts/01_Manager/LevelManager.cs
+++ CriesOfKowloonGroup01/Assets/06 Scripts/01_Manager/LevelManager.cs
@@ -20,12 +20,24 @@ public class LevelManager : MonoBehaviour
     {
         switch(stageName)
         {
-            case "SampleScene":
-            SceneManager.LoadScene(0);
+            case "Level 1-1":
+            SceneManager.LoadScene(2);
+            player.GetComponent<PlayerCombat>().lightDamage += 0;
+            player.GetComponent<PlayerCombat>().heavyDamage += 0;
+            player.GetComponent<PlayerHealth>().maxHealth += 0;
+            player.GetComponent<PlayerHealth>().currentHealth = player.GetComponent<PlayerHealth>().maxHealth;
             break;
 
-            case "Level 1":
-            SceneManager.LoadScene(0);
+            case "Level 1-2":
+            SceneManager.LoadScene(2);
+            player.GetComponent<PlayerCombat>().lightDamage += 5;
+            player.GetComponent<PlayerCombat>().heavyDamage += 2;
+            player.GetComponent<PlayerHealth>().maxHealth += 5;
+            player.GetComponent<PlayerHealth>().currentHealth = player.GetComponent<PlayerHealth>().maxHealth;
+            break;
+
+            case "Level 1-3":
+            SceneManager.LoadScene(2);
             player.GetComponent<PlayerCombat>().lightDamage += 10;
             player.GetComponent<PlayerCombat>().heavyDamage += 10;
             player.GetComponent<PlayerHealth>().maxHealth += 10;
