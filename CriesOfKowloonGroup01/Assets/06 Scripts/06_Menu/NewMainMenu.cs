3b7e4794bc849fc74031532790886e69d6692eac
Chan Wing Shen <0128682@kdu-online.com>
2022-04-15 23:25:38 +0800

    Update

--- CriesOfKowloonGroup01/Assets/06 Scripts/06_Menu/NewMainMenu.cs
+++ CriesOfKowloonGroup01/Assets/06 Scripts/06_Menu/NewMainMenu.cs
@@ -9,8 +9,11 @@ public class NewMainMenu : MonoBehaviour
     public GameObject controlList;
     public GameObject menuBackground;
     public GameObject backButton;
+    
     public void PlayGame()
     {
+        PlayerPrefs.SetInt("Upgraded", 0);
+        GameObject.FindGameObjectWithTag("Player").transform.position = new Vector2(2f, 7f);
         SceneManager.LoadScene(1);
     }
 
