e16366ff07381c3a78bbf0f9f615784b9ffa62fb
RandomWinter <85387387+RandomWinter@users.noreply.github.com>
2022-04-16 23:07:26 +0800

    Animation and Behavior Update E4 ~ 6

--- CriesOfKowloonGroup01/Assets/06 Scripts/04_Enemy/AccurateShoot.cs
+++ CriesOfKowloonGroup01/Assets/06 Scripts/04_Enemy/AccurateShoot.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 namespace _06_Scripts._04_Enemy {
     public class AccurateShoot : MonoBehaviour{
+        public GameObject target;
         public Transform launchOffset;
         public RangerBullet bullet;
         public GameObject personal;
+
+        private void Awake() {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
         
         private void OnEnable(){
-            Instantiate(bullet, launchOffset.position, transform.rotation);
+            Instantiate(bullet, launchOffset.position, target.transform.rotation);
         }
 
         private void OnDisable(){
