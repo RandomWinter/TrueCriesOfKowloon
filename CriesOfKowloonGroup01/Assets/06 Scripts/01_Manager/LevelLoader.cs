432d719f7b644afafbec4395cec310a11e72d497
Chan Wing Shen <0128682@kdu-online.com>
2022-04-18 20:19:18 +0800

    Update

--- CriesOfKowloonGroup01/Assets/06 Scripts/01_Manager/LevelLoader.cs
+++ CriesOfKowloonGroup01/Assets/06 Scripts/01_Manager/LevelLoader.cs
@@ -5,14 +5,81 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public int index;
+    public string stageName;
+
+    public void Awake()
+    {
+        var currentStage = SceneManager.GetActiveScene();
+        stageName = currentStage.name;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        switch(stageName)
         {
-            SceneManager.LoadScene(index);
-            other.transform.position = new Vector2(2f, 7f);
+            case "level 1-1":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(3);
+                other.transform.position = new Vector2(44f, 5f);
+            }
+            break;
+
+            case "level 1-2":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(4);
+                other.transform.position = new Vector2(2f, 7f);
+            }
+            break;
+
+            case "lvl 1-3":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(5);
+                other.transform.position = new Vector2(44f, 5f);
+            }
+            break;
+
+            case "lvl 2-1":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(6);
+                other.transform.position = new Vector2(2f, 7f);
+            }
+            break;
+
+            case "lvl 2-2":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(7);
+                other.transform.position = new Vector2(44f, 5f);
+            }
+            break;
+
+            case "lvl 2-3":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(8);
+                other.transform.position = new Vector2(2f, 7f);
+            }
+            break;
+
+            case "lvl 3-1":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(9);
+                other.transform.position = new Vector2(44f, 5f);
+            }
+            break;
+
+            case "lvl 3-2":
+            if(other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(10);
+                other.transform.position = new Vector2(2f, 7f);
+            }
+            break;
         }
     }
 }
